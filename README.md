6be68fdd8d1d4c1084131484a96aba5f72d7955f
terraputix <terraputix@mailbox.org>
2024-10-24 13:49:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Rust Bindings for Open-Meteo OmFileFormatC library
+
+This project provides bindings to the [OmFileFormatC](https://github.com/open-meteo/open-meteo/tree/dbc027b9257e6802444541dec0ed5b27a6bc7a0e/Sources/OmFileFormatC)  library.
+
+## Development
+
+Build and Test:
+
+```bash
+cargo build
+cargo test
+```
---
+++ build.rs
@@ -0,0 +1,60 @@
+use std::env;
+use std::ffi::OsStr;
+use std::path::PathBuf;
+use std::process::Command;
+
+const SUBMODULE: &str = "open-meteo/Sources/OmFileFormatC/";
+const LIB: &str = "ic";
+
+fn run_command<I, S>(prog: &str, args: I)
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    let output = Command::new(prog)
+        .args(args)
+        .output()
+        .expect("failed to start command; omfileformatc-rs currently only supports Unix");
+    assert!(output.status.success(), "{:?}", output);
+}
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=src/lib.rs");
+
+    let bindings = bindgen::Builder::default()
+        // fix strange cross compilation error from bindgen
+        // https://github.com/rust-lang/rust-bindgen/issues/1229
+        // for some reason setting sysroot to anything just works!?
+        // before that, I tried to set it conditionally based on target
+        // .clang_arg("--sysroot=/usr/aarch64-linux-gnu")
+        // .clang_arg("--sysroot=/usr/arm-linux-gnueabihf")
+        .clang_arg("--sysroot=/usr/aarch64-linux-gnu")
+        .header(format!("{}/include/vp4.h", SUBMODULE))
+        .header(format!("{}/include/fp.h", SUBMODULE))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+    let out_path_str = out_path.to_str().unwrap();
+
+    // We do the build in the out dir instead of in source, since it pollutes
+    // the lib directory.
+    // Copy the source files to the build directory and build the library.
+    // First we copy headers and C files from the submodule.
+    for item in [
+        format!("{}/{}", SUBMODULE, "src"),
+        format!("{}/{}", SUBMODULE, "include"),
+        "Makefile".to_string(),
+    ] {
+        run_command("cp", vec!["-R".to_string(), item, out_path_str.to_string()]);
+    }
+    run_command("make", vec!["-C", out_path_str, "libic.a"]);
+
+    println!("cargo:rustc-link-search=native={}", out_path_str);
+    println!("cargo:rustc-link-lib=static={}", LIB);
+}
