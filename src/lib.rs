4cc3a49837b1911e5e9f2e683461d29f6f0b045d
terraputix <terraputix@mailbox.org>
2024-10-29 12:02:53 +0100

    add some tests

--- src/lib.rs
+++ src/lib.rs
@@ -35,22 +35,20 @@ mod tests {
     }
 
     #[test]
-    fn turbo_pfor_roundtrip() {
+    fn test_round_trip_fp32_with_very_short_length() {
         let data: Vec<f32> = vec![10.0, 22.0, 23.0, 24.0];
         let length = 1; //data.len();
 
         // create buffers for compression and decompression!
-        let compressed_buffer = vec![0; 1000];
-        let compressed = compressed_buffer.as_slice();
-        let decompress_buffer = vec![0.0; length];
-        let decompressed = decompress_buffer.as_slice();
+        let mut compressed = vec![0; 1000];
+        let mut decompressed = vec![0.0; length];
 
         // compress data
         let compressed_size = unsafe {
             crate::fpxenc32(
                 data.as_ptr() as *mut u32,
                 length,
-                compressed.as_ptr() as *mut u8,
+                compressed.as_mut_ptr(),
                 0,
             )
         };
@@ -61,9 +59,9 @@ mod tests {
         // decompress data
         let decompressed_size = unsafe {
             crate::fpxdec32(
-                compressed.as_ptr() as *mut u8,
+                compressed.as_mut_ptr(),
                 length,
-                decompressed.as_ptr() as *mut u32,
+                decompressed.as_mut_ptr() as *mut u32,
                 0,
             )
         };
@@ -74,5 +72,79 @@ mod tests {
         // NOTE: This fails with 4 != 5 in the original turbo-pfor library
         assert_eq!(decompressed_size, compressed_size);
         assert_eq!(data[..length], decompressed[..length]);
+    }
+
+    #[test]
+    fn test_delta2d_decode() {
+        let mut buffer: Vec<i16> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        unsafe { crate::delta2d_decode(2, 5, buffer.as_mut_ptr()) };
+        assert_eq!(buffer, vec![1, 2, 3, 4, 5, 7, 9, 11, 13, 15]);
+    }
+
+    #[test]
+    fn test_delta2d_encode() {
+        let mut buffer: Vec<i16> = vec![1, 2, 3, 4, 5, 7, 9, 11, 13, 15];
+        unsafe { crate::delta2d_encode(2, 5, buffer.as_mut_ptr()) };
+        assert_eq!(buffer, vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    }
+
+    #[test]
+    fn test_delta2d_decode_xor() {
+        let mut buffer: Vec<f32> = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0];
+        unsafe { crate::delta2d_decode_xor(2, 5, buffer.as_mut_ptr()) };
+        let expected: Vec<f32> = vec![
+            1.0,
+            2.0,
+            3.0,
+            4.0,
+            5.0,
+            2.5521178e38,
+            2.0571151e-38,
+            3.526483e-38,
+            5.2897246e-38,
+            4.7019774e-38,
+        ];
+        assert_eq!(buffer, expected);
+    }
+
+    #[test]
+    fn test_delta2d_encode_xor() {
+        let mut buffer: Vec<f32> = vec![1.0, 2.0, 3.0, 4.0, 5.0, 7.0, 5.0, 11.0, 12.0, 15.0];
+        unsafe { crate::delta2d_encode_xor(2, 5, buffer.as_mut_ptr()) };
+        let expected: Vec<f32> = vec![
+            1.0,
+            2.0,
+            3.0,
+            4.0,
+            5.0,
+            2.9774707e38,
+            1.469368e-38,
+            4.4081038e-38,
+            7.052966e-38,
+            7.6407133e-38,
+        ];
+        assert_eq!(buffer, expected);
+    }
+
+    #[test]
+    fn test_delta2d_xor_roundtrip() {
+        let mut buffer: Vec<f32> = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0];
+        unsafe {
+            crate::delta2d_decode_xor(2, 5, buffer.as_mut_ptr());
+            crate::delta2d_encode_xor(2, 5, buffer.as_mut_ptr());
+        }
+        let expected: Vec<f32> = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0];
+        assert_eq!(buffer, expected);
+    }
+
+    #[test]
+    fn test_delta2d_roundtrip() {
+        let mut buffer: Vec<i16> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        unsafe {
+            crate::delta2d_decode(2, 5, buffer.as_mut_ptr());
+            crate::delta2d_encode(2, 5, buffer.as_mut_ptr());
+        }
+        let expected: Vec<i16> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        assert_eq!(buffer, expected);
     }
 }
