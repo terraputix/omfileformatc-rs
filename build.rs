a569a4c7f1fa7916825f3f1e49920040897fda8a
terraputix <terraputix@mailbox.org>
2024-11-01 21:08:18 +0100

    compiling as cdylib works

--- build.rs
+++ build.rs
@@ -138,11 +138,15 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings");
 
+    println!("cargo:warning=Writing bindings");
+
     // Write the bindings to the $OUT_DIR/bindings.rs file
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+
+    println!("cargo:warning=Bindings written to bindings.rs");
 
     // Link the static library
     // Link the object files and static library into a single WebAssembly binary
