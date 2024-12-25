5ea98f59c4594ca3138eabc31ac7ebfc62d340c4
terraputix <terraputix@mailbox.org>
2024-12-25 19:34:44 +0100

    update submodule

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,6 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
 mod tests {
-    use std::ffi::c_void;
-
-    use crate::{om_encoder_init, OmError_t_ERROR_OK};
-
     #[test]
     fn test_round_trip_p4n() {
         const n: usize = 3;
@@ -81,18 +77,18 @@ mod tests {
     #[test]
     fn test_delta2d_decode() {
         let mut buffer: Vec<i16> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        unsafe { crate::delta2d_decode(2, 5, buffer.as_mut_ptr()) };
+        unsafe { crate::delta2d_decode16(2, 5, buffer.as_mut_ptr()) };
         assert_eq!(buffer, vec![1, 2, 3, 4, 5, 7, 9, 11, 13, 15]);
     }
 
     #[test]
     fn test_delta2d_encode() {
         let mut buffer: Vec<i16> = vec![1, 2, 3, 4, 5, 7, 9, 11, 13, 15];
-        unsafe { crate::delta2d_encode(2, 5, buffer.as_mut_ptr()) };
+        unsafe { crate::delta2d_encode16(2, 5, buffer.as_mut_ptr()) };
         assert_eq!(buffer, vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
         let mut buffer: Vec<u8> = vec![2, 0, 3, 0, 7, 0, 8, 0];
-        unsafe { crate::delta2d_encode(4, 2, buffer.as_mut_ptr() as *mut i16) }
+        unsafe { crate::delta2d_encode16(4, 2, buffer.as_mut_ptr() as *mut i16) }
         assert_eq!(&buffer, &[2, 0, 3, 0, 5, 0, 5, 0])
     }
 
@@ -149,8 +145,8 @@ mod tests {
     fn test_delta2d_roundtrip() {
         let mut buffer: Vec<i16> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         unsafe {
-            crate::delta2d_encode(2, 5, buffer.as_mut_ptr());
-            crate::delta2d_decode(2, 5, buffer.as_mut_ptr());
+            crate::delta2d_encode16(2, 5, buffer.as_mut_ptr());
+            crate::delta2d_decode16(2, 5, buffer.as_mut_ptr());
         }
         let expected: Vec<i16> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         assert_eq!(buffer, expected);
